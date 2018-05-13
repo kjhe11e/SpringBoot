5c56c9cf4d15190ceb081733afbb2e1a1aa6c20d
Kyle Helle <kjhelle4@gmail.com>
2018-05-13 00:35:06 -0700

    initial commit

---
+++ src/main/java/com/example/basicspringboot/BasicSpringBoot/BasicSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.basicspringboot.BasicSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/basicspringboot/BasicSpringBoot/BasicSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.basicspringboot.BasicSpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
