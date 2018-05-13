6bf9da3e039d2331393a2e5f55efd5794422dff0
Kyle Helle <kjhelle4@gmail.com>
2018-05-13 00:56:42 -0700

    rename application.properties to application.yaml and add to .gitignore; create SocialAppAuthentication

---
+++ src/main/java/com/example/basicspringboot/BasicSpringBoot/SocialAppAuthentication.java
@@ -0,0 +1,9 @@
+package com.example.basicspringboot.BasicSpringBoot;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SocialAppAuthentication {
+    // TODO...
+}
